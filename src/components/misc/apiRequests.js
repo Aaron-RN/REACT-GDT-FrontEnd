9a17b3e4afaf65435e13caf7e5b54142369eef5c
Aaron-RN <aaron.rory.newbold@gmail.com>
2020-08-22 17:36:25 -0400

    Update apiRequests.js

--- src/components/misc/apiRequests.js
+++ src/components/misc/apiRequests.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = process.env.REACT_APP_API_URL;
+const API_URL = 'https://arn-gdt-assessment.netlify.app/';
 
 // Populate Musicians List
 const fetchMusicians = async (setLoading, setMusicians, setErrors) => {
